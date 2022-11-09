537ac5069b4f233bf3c247efff158ab8e3f46cfe
fkbeys <fkbeys@gmail.com>
2022-11-09 04:48:58 +0400

    1000 port

--- JenkinsDemo/Program.cs
+++ JenkinsDemo/Program.cs
@@ -7,16 +7,12 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
-
-var host = new WebHostBuilder()
-   .UseUrls("http://*:1000")  
-   .Build();
-
+ 
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+//if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
